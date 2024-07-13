9c1a3910fe37378ddcf2945dc4fe2ae13f2258ca
radiation-boi <prathamkhatri1995+github@gmail.comm>
2024-07-13 14:12:25 +0530

    dark and light theme game done

--- context-api-theme/src/context/UserContextProvider.jsx
+++ context-api-theme/src/context/UserContextProvider.jsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
+
+    
     const [user, setUser] = useState(null);
+
+
     return(
 <UserContext.Provider value={{user, setUser}}>
 {children}
